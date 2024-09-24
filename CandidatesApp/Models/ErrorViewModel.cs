e140f91b35b63b78a79878642661605b70c172a0
Brayan Burgos <brayanburgos1437@gmail.com>
2024-09-24 15:50:39 -0500

    CandidatesApp, MVC solution

---
+++ CandidatesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CandidatesApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
