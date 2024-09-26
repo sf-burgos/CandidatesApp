30688a9b6997a67e72d85063546a4077f5281aa0
Brayan Burgos <brayanburgos1437@gmail.com>
2024-09-26 10:57:47 -0500

    CandidatesApp, setting solution with CRQS and creating initial classes

---
+++ CandidatesApp/2.Aplication/DTOs/CandidateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CandidatesApp._2.Aplication.DTOs
+{
+    public class CandidateDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Email { get; set; }
+        public DateTime InsertDate { get; set; } = DateTime.Now;
+        public DateTime? ModifyDate { get; set; }
+
+        public ICollection<CandidateExperienceDTO> Experiences { get; set; }
+    }
+}
--- CandidatesApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CandidatesApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
