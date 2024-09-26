8856fa4fe11ceff4a12a84766c7ac85c3e88d5c3
Brayan Burgos <brayanburgos1437@gmail.com>
2024-09-26 12:03:07 -0500

    CandidatesApp, Creating classes for Commands and Queries (Candidates CRUD)

---
+++ CandidatesApp/3.Infrastructure/Commands/CreateCandidateCommand.cs
@@ -0,0 +1,10 @@
+﻿using CandidatesApp._2.Aplication.DTOs;
+using MediatR;
+
+namespace CandidatesApp._3.Infrastructure.Commands
+{
+    public record CreateCandidateCommand(string Name, string Surname, DateTime Birthday, string Email)
+        : IRequest<CandidateDTO>;
+
+  
+}
---
+++ CandidatesApp/3.Infrastructure/Queries/GetCandidateByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using CandidatesApp._2.Aplication.DTOs;
+using MediatR;
+
+namespace CandidatesApp._3.Infrastructure.Commands
+{
+    public record GetCandidateByIdQuery(int Id): IRequest<CandidateDTO>;
+    
+    
+}
